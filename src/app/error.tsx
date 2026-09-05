40b02d6a61a69f3b98910ec2c5c35be9f0f34e1c
Ye Yu <yeyuqwer@gmail.com>
2026-09-05 10:16:17 +0800

    🪛 chore: update all dependcies to the latest version

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import Link from 'next/link'
-import { Button } from '@/ui/shadcn/button'
+import { buttonVariants } from '@/ui/shadcn/button'
 
 // biome-ignore lint/suspicious/noShadowRestrictedNames: <ignore~>
 export default function Error() {
   return (
     <div className="flex flex-col items-center">
       <div className="text-3xl">Something went wrong!</div>
-      <Button render={<Link className="mt-4" href="/" />}>Back to home</Button>
+      <Link className={buttonVariants({ className: 'mt-4' })} href="/">
+        Back to home
+      </Link>
     </div>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
-import { Button } from '@/ui/shadcn/button'
+import { buttonVariants } from '@/ui/shadcn/button'
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center">
       <div className="text-3xl">Page not found.</div>
-      <Button render={<Link className="mt-4" href="/" />}>Back to home</Button>
+      <Link className={buttonVariants({ className: 'mt-4' })} href="/">
+        Back to home
+      </Link>
     </div>
   )
 }
